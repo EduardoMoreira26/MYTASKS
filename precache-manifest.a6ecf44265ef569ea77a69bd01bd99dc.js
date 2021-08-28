8ac9fe77d6d16764641eb96c82d7a760a6da9d67
EduardoMoreira26 <EduardoMoreira26@users.noreply.github.com>
2021-08-28 20:38:44 +0000

    deploy: 92c9af0f4988cd24721aa191c4826490d98e47b2

---
+++ precache-manifest.a6ecf44265ef569ea77a69bd01bd99dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaacfde1f46421d9c93b5f03a06c377",
+    "url": "https://EduardoMoreira26.github.io/MYTASKS/index.html"
+  },
+  {
+    "revision": "a68f31d845a76bc058e0",
+    "url": "https://EduardoMoreira26.github.io/MYTASKS/static/css/main.efc2adf4.chunk.css"
+  },
+  {
+    "revision": "70cc88acc8e375c3cc16",
+    "url": "https://EduardoMoreira26.github.io/MYTASKS/static/js/2.c6aa47bf.chunk.js"
+  },
+  {
+    "revision": "a68f31d845a76bc058e0",
+    "url": "https://EduardoMoreira26.github.io/MYTASKS/static/js/main.d467747e.chunk.js"
+  },
+  {
+    "revision": "ded6592ce42f13eb9ad1",
+    "url": "https://EduardoMoreira26.github.io/MYTASKS/static/js/runtime~main.43f94c92.js"
+  }
+]);
